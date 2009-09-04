8436c34d836771973844595dbbd028c0de2c5b4d
snakebyteme2 <snakebyteme2@e1e395e6-9584-11de-b6d4-8762f465f3f9>
2009-09-04 12:49:50 +0000

    Added ability to choose XPDF or GhostScript as the renderer. (Speed is the same since the PDF stream is not persisted)

--- PDFViewASP/AppCode/ASPPDFLib.vb
+++ PDFViewASP/AppCode/ASPPDFLib.vb
@@ -7,9 +7,8 @@
 #Region "Public Methods"
 
   Public Shared Function GetPageFromPDF(ByVal sourceFileName As String, ByVal destFolderPath As String, ByVal iPageNumber As Integer, Optional ByVal DPI As Integer = 0, Optional ByVal password As String = "", Optional ByVal rotations As Integer = 0) As String
-    Dim aspPDF As New ASPPDFLib
-    GetPageFromPDF = aspPDF.GetImageFromFile(sourceFileName, destFolderPath, iPageNumber - 1, DPI, password, rotations)
-    aspPDF = Nothing
+    GetPageFromPDF = AFPDFLibUtil.GetPageFromPDF(sourceFileName, destFolderPath, iPageNumber, DPI, password)
+    ImageUtil.ApplyRotation(GetPageFromPDF, rotations)
   End Function
 
   Public Shared Function GetPageCount(ByVal sourceFileName As String, Optional ByVal password As String = "") As Integer
@@ -28,20 +27,19 @@
     Return iTextSharpUtil.BuildASPBookmarkTreeFromPDF(sourceFileName, treeViewCol, password)
   End Function
 
-#End Region
 
-#Region "Private Methods"
-
-  Private Function GetImageFromFile(ByVal sFileName As String, ByVal destPath As String, ByVal iFrameNumber As Integer, Optional ByVal DPI As Integer = 0, Optional ByVal password As String = "", Optional ByVal rotations As Integer = 0) As String
-    GetImageFromFile = Nothing
+  Public Shared Function GetImageFromFileGS(ByVal sFileName As String, ByVal destPath As String, ByVal iFrameNumber As Integer, Optional ByVal DPI As Integer = 0, Optional ByVal password As String = "", Optional ByVal rotations As Integer = 0) As String
+    GetImageFromFileGS = Nothing
     If ImageUtil.IsPDF(sFileName) Then 'convert one frame to an image for viewing
-      GetImageFromFile = ConvertPDF.PDFConvert.GetPageFromPDF(sFileName, destPath, iFrameNumber + 1, DPI, password)
+      GetImageFromFileGS = ConvertPDF.PDFConvert.GetPageFromPDF(sFileName, destPath, iFrameNumber + 1, DPI, password)
     ElseIf ImageUtil.IsTiff(sFileName) Then
-      GetImageFromFile = ImageUtil.GetFrameFromTiff(sFileName, destPath, iFrameNumber)
+      GetImageFromFileGS = ImageUtil.GetFrameFromTiff(sFileName, destPath, iFrameNumber)
     End If
-    ImageUtil.ApplyRotation(GetImageFromFile, rotations)
+    ImageUtil.ApplyRotation(GetImageFromFileGS, rotations)
   End Function
 
 #End Region
+
+
 
 End Class
